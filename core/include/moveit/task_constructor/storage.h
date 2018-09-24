321da8874f4569009ea591c96fb049c335ea6339
v4hn <me@v4hn.de>
2018-09-24 16:28:06 +0200

    add comments to wrapped solutions

--- core/include/moveit/task_constructor/storage.h
+++ core/include/moveit/task_constructor/storage.h
@@ -243,8 +243,8 @@ public:
 	}
 
 protected:
-	SolutionBase(StagePrivate* creator = nullptr, double cost = 0.0)
-	   : creator_(creator), cost_(cost)
+	SolutionBase(StagePrivate* creator = nullptr, double cost = 0.0, std::string comment = "")
+	   : creator_(creator), cost_(cost), comment_(std::move(comment))
 	{
 	}
 
